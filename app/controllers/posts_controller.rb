b61fbad750401ba8f16c56158fea602d952131a7
vanyavi <vanyavi06@gmail.com>
2015-09-03 21:39:53 +0300

    it ruby magic. posts logic add

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+
+  	@post.save
+  	redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+  	@posts = Post.all.order('created_at DESC')
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
